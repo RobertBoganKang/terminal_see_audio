bee7c0a59637f833dc8a4b7d8109a4c10cd6f591
Robert Bogan Kang <bob.kang@qq.com>
2020-12-02 19:19:03 +0800

    add flower ground line

--- utils/analyze_common.py
+++ utils/analyze_common.py
@@ -25,6 +25,7 @@ class AnalyzeCommon(Common):
         self.flower_figure_size = (12, 12)
         self.flower_dpi = 200
         self.flower_line_width = 2
+        self.flower_ground_line_width = 2.5
         self.flower_baseline_width = 1.2
         self.flower_baseline_transform_alpha = 1.3
         self.flower_stem_power_coefficient = 1.5
@@ -35,6 +36,7 @@ class AnalyzeCommon(Common):
         # minimum source power
         self.flower_min_power = 0.001
         self.flower_min_analyze_power = 0.05
+        self.flower_min_angle_connection = 30
 
     def _analyze_log_min_max_transform(self, array, log=True, dynamic_max_value=False):
         if log:
@@ -252,3 +254,9 @@ class AnalyzeCommon(Common):
             # delete folder after use (file too big)
             shutil.rmtree(save_analyzer_path)
             return starting_time, ending_time, True
+
+    @staticmethod
+    def _analyze_get_angle(x0, x1, y0, y1):
+        angle_0 = np.arctan2(x0, y0)
+        angle_1 = np.arctan2(x1, y1)
+        return abs((angle_0 - angle_1 + np.pi) % (2 * np.pi) - np.pi)
--- utils/phase_analyzer.py
+++ utils/phase_analyzer.py
@@ -80,7 +80,17 @@ class PhaseAnalyzer(AnalyzeCommon):
                                          ratio_array[i],
                                          1)
                 ax.plot([x_positions[i], x_peaks[i]],
-                        [y_positions[i], y_peaks[i]], c=color, alpha=energy, zorder=2)
+                        [y_positions[i], y_peaks[i]], c=color,linewidth=self.flower_line_width, alpha=energy, zorder=2)
+                if i != 0 and self._analyze_get_angle(x_positions[i],
+                                                      x_positions[i - 1],
+                                                      y_positions[i],
+                                                      y_positions[i - 1]) \
+                        < self.flower_min_angle_connection / 180 * np.pi:
+                    ax.plot([x_positions[i], x_positions[i - 1]],
+                            [y_positions[i], y_positions[i - 1]], c=color,
+                            linewidth=self.flower_ground_line_width,
+                            alpha=(energies[i] + energies[i - 1]) / 2,
+                            zorder=2)
                 cir_end = Circle((x_peaks[i], y_peaks[i]), radius=energy / 5, zorder=3, facecolor=color,
                                  linewidth=self.flower_line_width, edgecolor=color, alpha=energy)
                 ax.add_patch(cir_end)
